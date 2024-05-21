0a2127f26e10e1be412afe5bfec03eb5728080a4
haoming-mmu <1191202158@student.mmu.edu.my>
2024-05-21 17:42:45 +0800

    app/page.tsx Route

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import type { Route } from 'next';
 
 export default function Page() {
   return (
@@ -22,7 +23,7 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href="/login" as Route
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
